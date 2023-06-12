214e56ec74fa3faf3a894bf3a35894223bb59ce1
AlejFernandezV <alejfernandez@unicauca.edu.co>
2023-06-12 16:52:58 -0500

    Changes in OpenMarketProducerApplication

--- springboot/openmarketProducer/src/main/java/com/unicauca/openmarketProducer/OpenmarketProducerApplication.java
+++ springboot/openmarketProducer/src/main/java/com/unicauca/openmarketProducer/OpenmarketProducerApplication.java
@@ -1,41 +1,12 @@
 package com.unicauca.openmarketProducer;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.unicauca.openmarketProducer.domain.service.OpenMarketRMQProducer;
-
 @SpringBootApplication
 public class OpenmarketProducerApplication {
-	private final OpenMarketRMQProducer OMProducer;
-	private final OpenMarketRMQConfig OMConfig;
-	private final static String QUEUE_NAME = "OM Products";
-
-	@Autowired
-    public OpenmarketProducerApplication(OpenMarketRMQProducer OMProducer) {
-        this.OMProducer = OMProducer;
-    }
-
-	public static void main(String[] args) {
-
-		ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost(spring.rabbitmq.host);
-
-        try (
-				Connection connection = factory.newConnection();
-				Channel channel = connection.createChannel()
-			) 
-		{
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            String message = argv.length > 0 ? String.join(" ", argv) : "default message";
-            channel.basicPublish("", QUEUE_NAME, null,
-                    message.getBytes(StandardCharsets.UTF_8));
-            System.out.println(" [x] Sent '" + message + "'");
-        }
-		//SpringApplication.run(OpenmarketApplication.class, args);
+	
+	public static void main(String[] argv) throws Exception{
+		SpringApplication.run(OpenmarketProducerApplication.class, argv);
 	}
-
-
-
 }
--- springboot/openmarketProducer/src/main/java/com/unicauca/openmarketProducer/domain/service/OpenMarketRMQProducer.java
+++ springboot/openmarketProducer/src/main/java/com/unicauca/openmarketProducer/domain/service/OpenMarketRMQProducer.java
@@ -1,26 +1,49 @@
 package com.unicauca.openmarketProducer.domain.service;
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.unicauca.openmarketProducer.utils.Constants;
+
 import com.unicauca.openmarketProducer.domain.entity.Product;
+import com.unicauca.openmarketProducer.utils.Constants;
 
 @Component
 public class OpenMarketRMQProducer {
-    private final RabbitTemplate rabbitTemplate;
-    private final Queue queue;
-    
+    private final static String QUEUE_NAME = "OM Products";
+    ConnectionFactory factory;
     @Autowired
-    public OpenMarketRMQProducer(RabbitTemplate rabbitTemplate, Queue queue) {
-        this.rabbitTemplate = rabbitTemplate;
-        this.queue = queue;
+    public OpenMarketRMQProducer() {
+        factory = new ConnectionFactory();
+        factory.setHost(Constants.HOST_NAME);
     }
-    
-    public void sendMessage(String action, Product product) {
-        String message = action+","+product.getId()+","+product.getName()+","+product.getPrice();
-        rabbitTemplate.convertAndSend(queue.getName(), message);
-        System.out.println("Mensaje enviado: Action:" +action+"| Product info: Id:"+product.getId()+
+
+    public void sendMessage(String action, Product product)throws Exception{
+        try (
+				Connection connection = factory.newConnection();
+				Channel channel = connection.createChannel();
+			) 
+		{
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+			String message = action+","+product.getId()+","+product.getName()+","+product.getPrice(); 
+			System.out.println("Informacion a enviar: Action:" +action+"| Product info: Id:"+product.getId()+
                             ", Name:"+product.getName()+", Price:"+product.getPrice());
+
+			if(message.length() < 0){
+				message = "default message";
+			} 
+
+            channel.basicPublish("", QUEUE_NAME, null,
+                    message.getBytes(StandardCharsets.UTF_8));
+
+            System.out.println(" [x] Sent '" + message + "'");
+			
+        }
     }
 }
