0a913f25c5c4f30d5d825fa8a810b10d41847dd8
Jojan-Esteban-Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2023-06-12 20:22:15 -0500

    Added the csv log consumer

---
+++ aoe/openmarket-csv-logs/src/main/java/com/unicauca/openmarket/access/repository/CsvEventLogRepository.java
@@ -0,0 +1,20 @@
+package com.unicauca.openmarket.access.repository;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CsvEventLogRepository implements IEventLogRepository {
+    @Override
+    public void appendRow(String row) {
+        String fileName = "events.csv";
+        row = row + "\n";
+
+        try (FileOutputStream fos = new FileOutputStream(fileName, true)) {
+            byte[] contentBytes = row.getBytes();
+            fos.write(contentBytes);
+            System.out.println("Saved an event.");
+        } catch (IOException e) {
+            System.err.println("An error occurred: " + e.getMessage());
+        }
+    }
+}
