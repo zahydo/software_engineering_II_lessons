67ae8727d11752098893692ea3e3c1fb44f37713
JhossefCons <jhniconstain@unicauca.edu.co>
2023-06-13 01:19:35 -0500

    ConsumerCSV v1

--- springboot/openmarketConsumerCSV/src/main/java/co/unicauca/openmarketconsumercsv/OpenmarketConsumerCSV.java
+++ springboot/openmarketConsumerCSV/src/main/java/co/unicauca/openmarketconsumercsv/OpenmarketConsumerCSV.java
@@ -8,9 +8,54 @@ package co.unicauca.openmarketconsumercsv;
  *
  * @author Alejandro, William y Jhossef
  */
-public class OpenmarketConsumerCSV {
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+import com.rabbitmq.client.Connection;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class OpenMarketRabbitMQConsumer {
+    private final String CSV_LOG_QUEUE = "csv-log-queue";
+    private final String PRODUCT_EVENTS_EXCHANGE = "product-events-exchange";
+    private ConnectionFactory factory;
+    private Connection connection;
+    private Channel channel;
+
+    private IEventLogService eventLogService;
+
+    public OpenMarketRabbitMQConsumer(IEventLogService eventLogService, String host, int port, String username, String password)
+            throws IOException, TimeoutException {
+        this.eventLogService = eventLogService;
+        this.factory = new ConnectionFactory();
+        this.factory.setHost(host);
+        this.factory.setPort(port);
+        this.factory.setUsername(username);
+        this.factory.setPassword(password);
+
+        this.connection = this.factory.newConnection();
+        this.channel = this.connection.createChannel();
+        this.channel.exchangeDeclare(PRODUCT_EVENTS_EXCHANGE, "fanout", true, false, null);
+        this.channel.queueDeclare(CSV_LOG_QUEUE, true, false, false, null);
+        this.channel.queueBind(CSV_LOG_QUEUE, PRODUCT_EVENTS_EXCHANGE, "");
+    }
+    
+    public void startConsuming() throws IOException {
+        // Crear el callback que se ejecutará cuando se reciba un mensaje
+        DeliverCallback deliverCallback = (consumerTag, message) -> {
+            String row = new String(message.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + row + "'");
+            this.eventLogService.appendRow(row);
+
+        };
+        // Iniciar el consumo de mensajes
+        this.channel.basicConsume(CSV_LOG_QUEUE, true, deliverCallback, consumerTag -> {
+        });
+    }
+
+    public void stopConsuming() throws IOException, TimeoutException {
+        // Cerrar la conexion 
+        this.channel.close();
+        this.connection.close();
     }
 }
